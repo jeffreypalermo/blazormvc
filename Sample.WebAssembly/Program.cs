beb5811e6e2b1b48ec5c92cfac078dd96454b2fc
Jeffrey Palermo <jeffrey@clear-measure.com>
2021-05-12 19:33:58 -0500

    simplified sample.

--- Sample.WebAssembly/Program.cs
+++ Sample.WebAssembly/Program.cs
@@ -14,10 +14,8 @@ namespace Sample.WebAssembly
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.Logging.SetMinimumLevel(LogLevel.Debug);
             builder.Services.AddScoped(
                 sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
-            builder.Services.AddSingleton<IUiBus>(provider => provider.GetService<MvcBus>());
             builder.Services.AddScoped<IUiBus>(provider => new MvcBus(NullLogger<MvcBus>.Instance));
 
             builder.RootComponents.Add<AppController>("#app");
