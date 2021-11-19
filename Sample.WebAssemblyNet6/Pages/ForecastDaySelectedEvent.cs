a98171de7df6756e2116e6cdcb0d0eac733c5e7e
Richard Ram <richard.ram@clear-measure.com>
2021-11-18 18:19:33 -0800

    Added .Net6 WebAssembly application with Blazor MVC Model

---
+++ Sample.WebAssemblyNet6/Pages/ForecastDaySelectedEvent.cs
@@ -0,0 +1,15 @@
+﻿using Palermo.BlazorMvc;
+using Sample.WebAssemblyNet6.Models;
+
+namespace Sample.WebAssemblyNet6.Pages
+{
+    public class ForecastDaySelectedEvent : IUiBusEvent
+    {
+        public WeatherForecast Forecast { get; }
+
+        public ForecastDaySelectedEvent(WeatherForecast forecast)
+        {
+            Forecast = forecast;
+        }
+    }
+}
